284f14a58bc64cb97448ba98636cd939a271b1c3
Xiao yi Hu <xy1998@live.nl>
2022-08-23 13:13:02 +0200

    added clamping on HDR and SDR conversion

--- source/imageWriting.hpp
+++ source/imageWriting.hpp
@@ -1,8 +1,8 @@
 #ifndef IMAGE_WRITING_HPP
 #define IMAGE_WRITING_HPP
 
+#include <cmath>
 #include "vec3.hpp"
-
 #ifdef _MSC_VER
     #pragma warning (push, 0); //disable warnings for stb_image lib for MSVC
 #endif 
@@ -77,13 +77,15 @@ inline void writeColor(std::ostream& out, std::vector<uint8_t>& imageData, const
 
 inline void convertSDRtoHDR(const std::vector<uint8_t>& sdrBuffer, std::vector<float>& hdrBuffer){
     for(int i = 0; i < hdrBuffer.size(); i++){
-        hdrBuffer[i] = static_cast<float>(sdrBuffer[i] / 255.999);
+        float hdrColor = static_cast<float>(sdrBuffer[i] / 255.999);
+        hdrBuffer[i] = (hdrColor >= 1.0f) ? 1.0f : hdrColor;
     }
 }
 
 inline void convertHDRtoSDR(const std::vector<float>& hdrBuffer, std::vector<uint8_t>& sdrBuffer){
     for(int i = 0; i < hdrBuffer.size(); i++){
-        sdrBuffer[i] = static_cast<uint8_t>(255.999 * hdrBuffer[i]);
+        int sdrColor = static_cast<uint8_t>(255.999 * hdrBuffer[i]);
+        sdrBuffer[i] = (sdrColor >= 255) ? 255 : sdrColor;
     }
 }
 
